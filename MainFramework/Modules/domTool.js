47a197d9e79f7e2acd51d70b15adb939ba6907c3
Lune <lolidrop@icloud.com>
2024-12-14 02:02:56 -0800

    update domTool.js

--- MainFramework/Modules/domTool.js
+++ MainFramework/Modules/domTool.js
@@ -75,8 +75,282 @@ const htmlTools = (() => {
 
         return div;
     }
-    return Object.seal({
 
+    function append(pos, eId) {
+        let element = createDiv(eId);
+
+        switch (pos) {
+        case 'before':
+            element = appendPatch(pos, eId);
+            break;
+        case 'after':
+            element = appendPatch(pos, eId);
+            break;
+        case 'beforemain':
+            document.getElementById('passage-content').insertAdjacentElement('afterbegin', element);
+            break;
+        case 'aftermain':
+            document.getElementById('passage-content').insertAdjacentElement('beforeend', element);
+            break;
+        }
+
+        return element;
+    }
+
+    /**
+     * @description replace the oldlink with newlink
+     * @param {string | language} oldlink
+     * @param {string} newlink
+     * @returns {HTMLElement}
+     * @example
+     * replaceLink('oldlink', '<<link newlink>><</link>>')
+     * replacelink({CN:'旧链接', EN:'oldlink'}, '<<link newlink>><</link>>')
+    */
+    function replaceLink(oldlink, newlink) {
+        if (typeof oldlink == 'object') {
+            oldlink = lanSwitch(oldlink);
+        }
+
+        // find the oldlink in elements
+        const links = document.getElementsByClassName('macro-link');
+        let elements;
+        for (let i = 0; i < links.length; i++) {
+            if (links[i].innerHTML.has(oldlink)) {
+                // replace the oldlink with newlink
+                elements = links[i];
+                break;
+            }
+        }
+        if (!elements) return;
+
+        // replace the oldlink with newlink
+        const parent = elements.parentNode;
+        const newlinkPatch = document.createElement('div');
+        newlinkPatch.id = 'patchlink';
+        parent.replaceChild(newlinkPatch, elements);
+
+        // wikifier the newlink
+        new Wikifier(null, `<<replace "#patchlink">>${newlink}<</replace>>`);
+
+        return newlinkPatch;
+    }
+
+
+    function _wikify(eId = 'patchContent', content) {
+        new Wikifier(null, `<<append "#${eId}">>${content}<</append>>`);
+    }
+
+    /**
+     * @description append content to certain element.
+     * first parameter is the type of element, it can be 'text', 'link', 'div', 'span', 'u', 'b', 'i'
+     * second parameter is the id of the element or the text of the element
+     * third parameter is the content to append
+     * fourth parameter can set the id of the new div
+     * @param {string} tag
+     * @param {string | language | LangType} txt
+     * @param {string} content
+     * @example
+     * appendTo('text', 'you're townie.', '<<anywidget>> or text')
+     * - will append a new div with content after the specified text node
+     * appendTo('link', ['ENLinkText', 'CNLinkText'], '<<anywidget>> or text')
+     * - will append a new div with content after the specified link
+     * appendTo('div', 'divId', '<<anywidget>> or text')
+     * - will append a new div with content after the specified div
+     */
+    function appendTo(tag, txt, content, divId = null) {
+        const eType = tag;
+        const eId = txt;
+        const count = this.applyCount + 1;
+        const dId = divId === null ? `patch${tag}${count}` : divId;
+
+        // if the tag is a text node
+        if (eType === 'text') {
+            const textNodes = document.getElementById('passage-content').childNodes;
+            const txt = lanSwitch(eId);
+            let node;
+            for (let i = 0; i < textNodes.length; i++) {
+                if (textNodes[i].textContent.has(txt)) {
+                    node = textNodes[i];
+                    break;
+                }
+            }
+            // append the content after the text node
+            const div = createDiv(dId);
+            node.parentNode.insertBefore(div, node.nextSibling);
+            _wikify(dId, content);
+        }
+        else if (eType === 'link') {
+            const links = document.getElementsByClassName('macro-link');
+            const txt = lanSwitch(eId);
+            let node;
+            for (let i = 0; i < links.length; i++) {
+                if (links[i].textContent.has(txt)) {
+                    node = links[i];
+                    break;
+                }
+            }
+            // append the content after the link
+            const div = createDiv(dId);
+            node.parentNode.insertBefore(div, node.nextSibling);
+            _wikify(dId, content);
+        }
+        else if (eType === 'span' || eType === 'u' || eType === 'b' || eType === 'i') {
+            let nodes;
+            switch (eType) {
+            case 'span':
+                nodes = document.getElementsByTagName('span');
+                break;
+            case 'u':
+                nodes = document.getElementsByTagName('u');
+                break;
+            case 'b':
+                nodes = document.getElementsByTagName('b');
+                break;
+            case 'i':
+                nodes = document.getElementsByTagName('i');
+                break;
+            }
+
+            const txt = lanSwitch(eId);
+            let node;
+            for (let i = 0; i < nodes.length; i++) {
+                if (nodes[i].textContent.has(txt)) {
+                    node = nodes[i];
+                    break;
+                }
+            }
+            // append the content after the span
+            const div = createDiv(dId);
+            node.insertAdjacentElement('afterend', div);
+            _wikify(dId, content);
+        }
+        else if (eId) {
+            const element = document.getElementById(eId);
+            if (!element) {
+                throw new Error(`Element with id ${eId} not found`);
+            }
+
+            // append the content after the element
+            const div = this.createDiv(`patch${eId}${count}`);
+            element.insertAdjacentElement('afterend', div);
+            this._wikify(`patch${eId}${count}`, content);
+        }
+        else {
+            throw new Error('Invalid element type');
+        }
+    }
+
+    /**
+     * @description insert content before/after certain link
+     * @param {'before' | 'after'} position;
+     * @param {string} eId;
+     * @param {string | language} link;
+     * @param {string} content;
+     * @returns {HTMLElement}
+     * @example
+     * insertToLink({link: 'linktext', content:'<<anywidget>>'})
+     * //will insert a new div with content before the certain link
+     * //the default id of new div is 'patchContent'
+     *
+     * insertToLink({pos: 'after', link: 'linktext', content:'<<anywidget>>'})
+     * //will insert a new div with content after the certain link
+     *
+     * insertToLink({eId: 'myContent', link: 'linktext', content:'<<anywidget>>'})
+     * //'myContent' is the id of the new div
+     */
+    function insertToLink(options) {
+        if (typeof options.link == 'object') {
+            link = lanSwitch(options.link);
+        }
+
+        const doc = document.getElementById('passage-content');
+        const links = doc.getElementsByClassName('macro-link');
+        let element;
+        for (let i = 0; i < links.length; i++) {
+            if (links[i].innerHTML.has(options.link)) {
+                element = links[i];
+                break;
+            }
+        }
+        if (!element) return;
+
+        // check div if already exist, if not then create a new one
+        let div = document.getElementById(options.eId);
+        const eID = options.eId ?? 'patchContent';
+    
+        if (!div) {
+            div = document.createElement('div');
+            div.id = eID;
+        }
+
+        let pos = 'beforebegin';
+        if (options.pos == 'after') {
+            pos = 'afterend';
+        }
+
+        // if position is before then check the link previous sibling is image or not
+        // if so then insert before the image
+        if (pos == 'beforebegin') {
+            if (element.previousElementSibling.tagName == 'IMG') {
+                element = element.previousElementSibling;
+            }
+        }
+
+        element.insertAdjacentElement(pos, div);
+
+        // wikifier the content
+        new Wikifier(null, `<<append "#${eID}">>${options.content ?? ''}<</append>>`);
+
+        return div;
+    }
+
+    /**
+     * @description replace text at TEXT_NODE
+     * @param {string | language} oldtext
+     * @param {string | language} newtext
+     * @example
+     * replaceText('oldtext', 'newtext')
+     * replaceText({CN:'旧文本', EN:'oldtext'}, 'newtext')
+     * replaceText('oldtext', {CN:'新文本', EN:'newtext'})
+     */
+    function replaceText(oldtext, newtext) {
+        const doc = document.getElementById('passage-content');
+        const textNodes = doc.childNodes;
+        if (!textNodes) return;
+
+        if (typeof oldtext == 'object') {
+            oldtext = lanSwitch(oldtext);
+        }
+
+        if (typeof newtext == 'object') {
+            newtext = lanSwitch(newtext);
+        }
+
+        for (let i = 0; i < textNodes.length; i++) {
+            const node = textNodes[i];
+            if (node.textContent.has(oldtext)) {
+                node.textContent = node.textContent.replace(oldtext, newtext);
+                break;
+            }
+        }
+    }
+
+    return Object.seal({
+        _count : 0,
+        get applyCount() {
+            return this._count ?? 0;
+        },
+        set applyCount(value) {
+            this._count = value;
+        },
+        isIconImg,
+        appendPatch,
+        append,
+        replaceLink,
+        wikify : _wikify,
+        appendTo,
+        insertToLink,
+        replaceText
     });
 })();
 
